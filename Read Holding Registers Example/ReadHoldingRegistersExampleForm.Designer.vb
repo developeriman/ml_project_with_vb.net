a529a931a47c3f76174dc25aa139ba0b96c5f569
ImamHossen <imanhossain914@gmail.com>
2022-04-28 12:53:20 +0600

    Visual basic machine leaning project completed

---
+++ Read Holding Registers Example/ReadHoldingRegistersExampleForm.Designer.vb
@@ -0,0 +1,184 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class ReadHoldingRegistersExampleForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(ReadHoldingRegistersExampleForm))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.lblMyTime = New System.Windows.Forms.Label()
+        Me.txtResult = New System.Windows.Forms.TextBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.txtReceiMsg = New System.Windows.Forms.TextBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txtSendMsg = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Button2)
+        Me.GroupBox1.Controls.Add(Me.lblMyTime)
+        Me.GroupBox1.Controls.Add(Me.txtResult)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.txtReceiMsg)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.txtSendMsg)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(738, 286)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Nhimex Fuel Manager"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(657, 21)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(75, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Minimize"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'lblMyTime
+        '
+        Me.lblMyTime.AutoSize = True
+        Me.lblMyTime.Location = New System.Drawing.Point(504, 245)
+        Me.lblMyTime.Name = "lblMyTime"
+        Me.lblMyTime.Size = New System.Drawing.Size(0, 16)
+        Me.lblMyTime.TabIndex = 0
+        '
+        'txtResult
+        '
+        Me.txtResult.Location = New System.Drawing.Point(124, 107)
+        Me.txtResult.Multiline = True
+        Me.txtResult.Name = "txtResult"
+        Me.txtResult.Size = New System.Drawing.Size(397, 117)
+        Me.txtResult.TabIndex = 1
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(50, 107)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(48, 16)
+        Me.Label3.TabIndex = 0
+        Me.Label3.Text = "Result:"
+        '
+        'txtReceiMsg
+        '
+        Me.txtReceiMsg.Location = New System.Drawing.Point(124, 79)
+        Me.txtReceiMsg.Name = "txtReceiMsg"
+        Me.txtReceiMsg.Size = New System.Drawing.Size(397, 22)
+        Me.txtReceiMsg.TabIndex = 1
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 79)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(72, 16)
+        Me.Label2.TabIndex = 0
+        Me.Label2.Text = "ReceiMsg:"
+        '
+        'txtSendMsg
+        '
+        Me.txtSendMsg.Location = New System.Drawing.Point(124, 51)
+        Me.txtSendMsg.Name = "txtSendMsg"
+        Me.txtSendMsg.Size = New System.Drawing.Size(397, 22)
+        Me.txtSendMsg.TabIndex = 1
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 51)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(68, 16)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "SendMsg:"
+        '
+        'Timer1
+        '
+        Me.Timer1.Enabled = True
+        Me.Timer1.Interval = 1
+        '
+        'Timer2
+        '
+        Me.Timer2.Enabled = True
+        Me.Timer2.Interval = 1
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(587, 110)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(48, 16)
+        Me.Label4.TabIndex = 5
+        Me.Label4.Text = "Label4"
+        '
+        'Timer3
+        '
+        Me.Timer3.Enabled = True
+        Me.Timer3.Interval = 10
+        '
+        'ReadHoldingRegistersExampleForm
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(763, 310)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "ReadHoldingRegistersExampleForm"
+        Me.Text = "Nhimex.com"
+        Me.TopMost = True
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents txtResult As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents txtReceiMsg As TextBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txtSendMsg As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents lblMyTime As Label
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Timer3 As Timer
+End Class
---
+++ Read Holding Registers Example/ReadHoldingRegistersExampleForm.vb
@@ -0,0 +1,278 @@
+﻿Imports System.IO.Ports
+Imports System.Threading
+Imports MySql.Data.MySqlClient
+Imports Microsoft.Office.Interop
+
+Public Class ReadHoldingRegistersExampleForm
+    Dim MysqlConn As MySqlConnection
+
+    Dim COMMAND As MySqlCommand
+
+
+    'Declare variablses & constants 
+    Private serialPort As SerialPort = Nothing
+    Private Sub ReadHoldingRegistersExampleForm_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            serialPort = New SerialPort("COM7", 9600, Parity.None, 8, StopBits.One)
+            serialPort.Open()
+        Catch ex As Exception
+            ' MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub ReadHoldingRegistersExampleForm_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        Try
+            serialPort.Close()
+        Catch ex As Exception
+            ' MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub btnReadHoldingRegisters_Click(sender As Object, e As EventArgs)
+        Try
+            Dim slaveAddress As Byte = 1
+            Dim functionCode As Byte = 3
+            Dim startAddress As UShort = 32784
+            Dim numberOfPoints = 17
+
+            Dim frame As Byte() = Me.ReadHolingRegisters(slaveAddress, functionCode, startAddress, numberOfPoints)
+            txtSendMsg.Text = Me.DisplayValue(frame)
+            serialPort.Write(frame, 0, frame.Length)
+            Thread.Sleep(100)
+
+
+            If serialPort.BytesToRead > 5 Then
+                Dim buffRecei As Byte() = New Byte(serialPort.BytesToRead) {}
+                serialPort.Read(buffRecei, 0, buffRecei.Length)
+                txtReceiMsg.Text = Me.DisplayValue(buffRecei)
+
+                Dim data As Byte() = New Byte(buffRecei.Length - 5) {}
+                Array.Copy(buffRecei, 3, data, 0, data.Length)
+                Dim result As UInt16() = DataType.Word.ToArray(data) '
+
+                'Display Result
+                txtResult.Text = String.Empty
+                For Each item As UInt16 In result
+                    txtResult.Text += String.Format("{0}", item)
+                Next
+
+
+            End If
+
+        Catch ex As Exception
+            'MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    ''' <summary>
+    ''' Read Holding registers
+    ''' </summary>
+    ''' <param name="slaveAddress"> Salve Address</param>
+    ''' <param name="functionCode"> Functon</param>
+    ''' <param name="startAddress"> Starting Address</param>
+    ''' <param name="numberOfPoints"> Quantity of Registers</param>
+    ''' <returns>Byte()</returns>
+    ''' <remarks> </remarks>
+    Private Function ReadHolingRegisters(slaveAddress As Byte, functionCode As Byte, startAddress As UShort, numberOfPoints As UShort) As Byte()
+        Dim frame As Byte() = New Byte(7) {}
+        frame(0) = slaveAddress
+        frame(1) = functionCode
+        frame(2) = CByte(startAddress / 256)
+        frame(3) = CByte(startAddress Mod 256)
+        frame(4) = CByte(numberOfPoints >> 8)
+        frame(5) = CByte(numberOfPoints)
+        Dim crc As Byte() = Me.CRC(frame)
+        frame(6) = crc(0)
+        frame(7) = crc(1)
+        Return frame
+
+    End Function
+
+    Private Function CRC(data As Byte()) As Byte()
+        Dim CRCFull As UShort = &HFFFF
+        Dim CRCHigh As Byte = &HF, CRCLow As Byte = &HFF
+        Dim CRCLSB As Char
+        Dim result As Byte() = New Byte(1) {}
+        For i As Integer = 0 To (data.Length) - 3
+            CRCFull = CUShort(CRCFull Xor data(i))
+
+            For j As Integer = 0 To 7
+                CRCLSB = ChrW(CRCFull And &H1)
+                CRCFull = CUShort((CRCFull >> 1) And &H7FFF)
+                If Convert.ToInt32(CRCLSB) = 1 Then
+                    CRCFull = CUShort(CRCFull Xor &HA001)
+                End If
+            Next
+        Next
+        CRCHigh = CByte((CRCFull >> 8) And &HFF)
+        CRCLow = CByte((CRCFull And &HFF))
+        CRCLow = CByte(CRCFull And &HFF)
+        Return New Byte(1) {CRCLow, CRCHigh}
+    End Function
+    Private Function DisplayValue(values As Byte()) As String
+        Dim result As String = String.Empty
+        For Each item As Byte In values
+            result += String.Format("{0:X2}", item)
+        Next
+        Return result
+    End Function
+
+    Private Sub RegisterTime_Tick(sender As Object, e As EventArgs)
+        Try
+            Dim slaveAddress As Byte = 1
+            Dim functionCode As Byte = 3
+            Dim startAddress As UShort = 32784
+            Dim numberOfPoints = 1
+
+            Dim frame As Byte() = Me.ReadHolingRegisters(slaveAddress, functionCode, startAddress, numberOfPoints)
+            txtSendMsg.Text = Me.DisplayValue(frame)
+            serialPort.Write(frame, 0, frame.Length)
+            Thread.Sleep(100)
+
+
+            If serialPort.BytesToRead > 5 Then
+                Dim buffRecei As Byte() = New Byte(serialPort.BytesToRead) {}
+                serialPort.Read(buffRecei, 0, buffRecei.Length)
+                txtReceiMsg.Text = Me.DisplayValue(buffRecei)
+
+                Dim data As Byte() = New Byte(buffRecei.Length - 5) {}
+                Array.Copy(buffRecei, 3, data, 0, data.Length)
+                Dim result As UInt16() = DataType.Word.ToArray(data) '
+
+                'Display Result
+                txtResult.Text = String.Empty
+                For Each item As UInt16 In result
+                    txtResult.Text += String.Format("{0}", item)
+                Next
+
+            End If
+
+        Catch ex As Exception
+            ' MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs)
+
+        MysqlConn = New MySqlConnection
+        MysqlConn.ConnectionString = "server ='localhost';userid='root';database='database';password=''"
+        Dim READER As MySqlDataReader
+
+        Try
+            '  Dim subst As String = txtReceiMsg.Text.Substring(0, 6)
+            '  If subst = 10311 Then
+            MysqlConn.Open()
+            Dim Query As String
+            Query = "insert into mydata(treceimsg,tresult)
+     values ('" & txtReceiMsg.Text & "','" & txtResult.Text & "')"
+            COMMAND = New MySqlCommand(Query, MysqlConn)
+            READER = COMMAND.ExecuteReader
+
+            ' MessageBox.Show("Data successfully insert to Database")
+            MysqlConn.Close()
+            '   End If
+        Catch ex As Exception
+            ' MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        Try
+            Dim slaveAddress As Byte = 1
+            Dim functionCode As Byte = 3
+            Dim startAddress As UShort = 32784
+            Dim numberOfPoints = 17
+
+            Dim frame As Byte() = Me.ReadHolingRegisters(slaveAddress, functionCode, startAddress, numberOfPoints)
+            txtSendMsg.Text = Me.DisplayValue(frame)
+            serialPort.Write(frame, 0, frame.Length)
+            Thread.Sleep(100)
+
+
+            If serialPort.BytesToRead > 5 Then
+                Dim buffRecei As Byte() = New Byte(serialPort.BytesToRead) {}
+                serialPort.Read(buffRecei, 0, buffRecei.Length)
+                txtReceiMsg.Text = Me.DisplayValue(buffRecei)
+
+                Dim data As Byte() = New Byte(buffRecei.Length - 5) {}
+                Array.Copy(buffRecei, 3, data, 0, data.Length)
+                Dim result As UInt16() = DataType.Word.ToArray(data) '
+
+                'Display Result
+                txtResult.Text = String.Empty
+                For Each item As UInt16 In result
+                    txtResult.Text += String.Format("{0}", item)
+                Next
+
+                '  Process.Start("http://localhost/apitest?msg=" & txtSendMsg.Text & "&txtReceiMsg=" &
+                ' txtReceiMsg.Text &
+                ' "&txtResult=" & txtResult.Text)
+
+            End If
+
+        Catch ex As Exception
+            ' MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        MysqlConn = New MySqlConnection
+        MysqlConn.ConnectionString ="server ='localhost';userid='root';database='database';password=''"
+        ' "server ='nhimex-prod.c0hfebe8dpuc.ap-southeast-1.rds.amazonaws.com';userid='nhimexAtiqDev';database='nhimex';password='nhimexdev2020'"
+        Dim READER As MySqlDataReader
+
+        Try
+
+
+            If ((txtReceiMsg.Text.Length >= 32) And (txtReceiMsg.Text.Length <= 40) And (txtResult.Text.Length <= 12)) Then
+                Dim subst As String = txtReceiMsg.Text.Substring(0, 6)
+                '(txtReceiMsg.Text.Length <= 10) And
+                If subst = 10311 Then
+
+                    Dim intCount As String = 9999999
+
+                    If (txtResult.Text <= intCount) Then
+                        Dim intCounts As String = txtResult.Text / 1000000
+
+                        Dim Filtering_number As String = 65.536
+
+                        If (intCounts <= Filtering_number) Then
+                            '   Dim numberOfRecords As String
+                            '   numberOfRecords = "Select tresult from mydata where tresult =' " & txtResult.Text & " " '"
+                            '   If numberOfRecords <> 0 Then
+                            '     MessageBox.Show("Data ")
+
+                            ' Else
+                            MysqlConn.Open()
+                            Dim Query As String
+                            Label4.Text = Date.Now.ToString("dd MMM yyyy hh:mm:ss")
+                            Query = "insert into mydata(treceimsg,tresult,DateTime)
+                        values ('" & txtReceiMsg.Text & "','" & intCounts & "' ,'" & Label4.Text & "')"
+                            COMMAND = New MySqlCommand(Query, MysqlConn)
+                            READER = COMMAND.ExecuteReader
+
+                            MysqlConn.Close()
+                        End If
+                    End If
+                End If
+            End If
+            ' End If
+        Catch ex As Exception
+            ' MessageBox.Show(Me, ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Me.WindowState = FormWindowState.Minimized
+    End Sub
+
+
+
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        Label4.Text = Date.Now.ToString("dd MMM yyyy hh:mm:ss")
+    End Sub
+End Class
